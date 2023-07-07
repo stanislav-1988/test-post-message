e777fab753c90aea8e15b55153b6527c3213afeb
Станислав <103958086+stanislav-1988@users.noreply.github.com>
2023-07-07 14:19:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-if (window.webkit.messageHandlers.pageDidLoad) {
-			window.addEventListener('load', () => window.webkit.messageHandlers.pageDidLoad.postMessage(JSON.stringify({'message' : 'load'})));
-			}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter >
