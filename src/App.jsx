1fdeb3f2340b4d3b8389e4e5b484b96616484c36
Станислав <103958086+stanislav-1988@users.noreply.github.com>
2023-07-07 14:19:48 +0300

    useEfect App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,25 @@ import Timer from './components/Timer';
 import People from './components/People';
 import logo from './logo.svg';
 
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 
 const  App = () => {
-    useLayoutEffect(() => {
-		
-		if (window.webkit) {
-			if (window.webkit.messageHandlers.pageWillUnload) {
-		  window.addEventListener('unload', () => window.webkit.messageHandlers.pageWillUnload.postMessage(JSON.stringify({'message' : 'unload'})));
-			}
-			if (window.webkit.messageHandlers.pageResized) {
-		  window.addEventListener('resize', () => window.webkit.messageHandlers.pageResized.postMessage(JSON.stringify({'message' : 'resize'})));
-			}
-			if (window.webkit.messageHandlers.pageDidHistoryNavigation) {
-		  window.addEventListener('popstate', () => window.webkit.messageHandlers.pageDidHistoryNavigation.postMessage(JSON.stringify({'message' : 'popstate'})));
-			}
-		}
-	
-		return () => {
-		  window.removeEventListener('load', () => window.webkit.messageHandlers.pageDidLoad.postMessage(JSON.stringify({'message' : 'load'})));
-		  window.removeEventListener('unload', () => window.webkit.messageHandlers.pageWillUnload.postMessage(JSON.stringify({'message' : 'unload'})));
-		  window.removeEventListener('resize', () => window.webkit.messageHandlers.pageResized.postMessage(JSON.stringify({'message' : 'resize'})));
-		  window.removeEventListener('popstate', () => window.webkit.messageHandlers.pageDidHistoryNavigation.postMessage(JSON.stringify({'message' : 'popstate'})));
-		};
-	  }, []);
+    useEffect(() => {
+    if (window.webkit) {
+      window.webkit.messageHandlers.rt.postMessage('load');
+      window.addEventListener('unload', handlerPostMessageUnLoad);
+      window.addEventListener('resize', handlerPostMessageResize);
+      window.addEventListener('popstate', handlerPostMessagePopstate);
+    }
+
+    return () => {
+      // window.removeEventListener('load', handlerPostMessageLoad);
+      window.removeEventListener('unload', handlerPostMessageUnLoad);
+      window.removeEventListener('resize', handlerPostMessageResize);
+      window.removeEventListener('popstate', handlerPostMessagePopstate);
+    };
+  }, []);
 	  return (
 		<div>
 			<header className='header'>
