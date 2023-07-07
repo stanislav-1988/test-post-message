ef751b5679eae03e92871614b99a264611c7d973
Станислав <103958086+stanislav-1988@users.noreply.github.com>
2023-07-07 14:27:38 +0300

    Update and rename index.js to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
 root.render(
-  <BrowserRouter >
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-    
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
